ba853dcd20aec3c4d3e45b944a4df2c68f57e149
Oliwia <oliwia.m.96@wp.pl>
2018-01-12 18:20:39 +0100

    Add wstaw_pracownika procedure

--- PLSQLFunctions.sql
+++ PLSQLFunctions.sql
@@ -16,5 +16,21 @@ BEGIN
 END;
 /
 
+create or replace PROCEDURE
+    wstaw_pracownika (v_imie IN pracownicy.imie%TYPE, v_nazwisko IN pracownicy.nazwisko%TYPE,
+                      v_plec IN pracownicy.plec%TYPE, v_id OUT pracownicy.id%TYPE) IS
+BEGIN
+    v_id := pracownik_id_seq.nextval;
+    INSERT INTO PRACOWNICY(ID, IMIE, NAZWISKO, PLEC)
+    VALUES(v_id, v_imie, v_nazwisko, v_plec);
+
+END wstaw_pracownika;
+
+
+DECLARE
+        v_zm pracownicy.id%TYPE;
+BEGIN
+      wstaw_pracownika('oliwia', 'masian', 'K', v_zm);
+END;
 
 
